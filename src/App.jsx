92d4c49ccc455388961d61991876c22209345950
Ansh Prajapat <anshprajapat8@gmail.com>
2025-05-23 14:18:54 +0530

    adds responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -3,63 +3,67 @@ import { useState } from 'react'
 function App() {
     const [tasks, setTasks] = useState([])
 
-    function handleAddTask(){
+    function handleAddTask(e) {
+        e.preventDefault()
         let taskValue = document.getElementById('task-input').value
-        if(taskValue.trim() !== ""){
+        if (taskValue.trim() !== "") {
 
             setTasks(t => [...t, taskValue])
-    
+
             /* to clear input field after click */
             let taskDOM = document.getElementById('task-input')
             taskDOM.value = ''
-            
+
         }
     }
-    function deleteTask(index){
-        const updatedTasks = t => t.filter( (_, i) => i!=index )
+    function deleteTask(index) {
+        const updatedTasks = t => t.filter((_, i) => i != index)
         setTasks(updatedTasks)
     }
-    function moveTaskUp(index){
-        if(index > 0){
+    function moveTaskUp(index) {
+        if (index > 0) {
             const updatedTasks = [...tasks];
 
-            [updatedTasks[index], updatedTasks[index - 1]] = 
-            [updatedTasks[index - 1], updatedTasks[index]]
+            [updatedTasks[index], updatedTasks[index - 1]] =
+                [updatedTasks[index - 1], updatedTasks[index]]
 
             setTasks(updatedTasks)
         }
     }
-    function moveTaskDown(index){
-        if(index < tasks.length - 1){
+    function moveTaskDown(index) {
+        if (index < tasks.length - 1) {
             const updatedTasks = [...tasks];
 
-            [ updatedTasks[index], updatedTasks[index + 1] ] = [ updatedTasks[index + 1], updatedTasks[index] ]
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]]
 
             setTasks(updatedTasks)
         }
     }
 
-  return (
-    <>
-        <h1>To-do List</h1>
-        <div className='adders'>
-            <input type="text" id='task-input'/>
-            <button className='add-btn' onClick={handleAddTask}>Add Task</button>
-        </div>
+    return (
+        <>
+            <h1>To-do List</h1>
+            <div className='adders'>
+                <form onSubmit={handleAddTask}>
+                    <input type="text" id='task-input' />
+                    <button className='add-btn' onSubmit={handleAddTask}>Add Task</button> 
+                </form>
+            </div>
 
-        <ol className="task-container">
-            {tasks.map( (task, index)=> <li key={index}>
-                <span className='task-text'>{task}</span>
-                <div>
-                    <button className='del-btn' onClick={()=>deleteTask(index)}>Delete</button>
-                    <button className='move-btn' onClick={()=> moveTaskUp(index)}>👆</button>
-                    <button className='move-btn' onClick={()=> moveTaskDown(index)}>👇</button>
-                </div>
-            </li> )}
-        </ol>
-    </>
-      
-  )
+
+            <ol className="task-container">
+                {tasks.map((task, index) => <li key={index}>
+                    <span className='task-text'>{task}</span>
+                    <div>
+                        <button className='del-btn' onClick={() => deleteTask(index)}>Delete</button>
+                        <button className='move-btn' onClick={() => moveTaskUp(index)}>👆</button>
+                        <button className='move-btn' onClick={() => moveTaskDown(index)}>👇</button>
+                    </div>
+                </li>)}
+            </ol>
+        </>
+
+    )
 }
 
 export default App
